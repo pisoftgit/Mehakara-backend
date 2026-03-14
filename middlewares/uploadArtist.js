6ed456b3ee6a7d190614a2fc436902e3d8405604
pisoftgit <pisoftgit@gmail.com>
2026-03-14 17:08:36 +0530

    artist upload

--- middlewares/uploadArtist.js
+++ middlewares/uploadArtist.js
@@ -1,27 +1,25 @@
-// middlewares/uploadArtist.js
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-// Ensure uploads folder exists
-const uploadPath = path.join(__dirname, '../uploads/artists');
-if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath, { recursive: true });
-
-// Configure multer storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    const userId = req.user._id.toString();
+    const uploadPath = path.join(__dirname, `../uploads/artist/${userId}`);
+    fs.mkdirSync(uploadPath, { recursive: true });
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
+    const ext = path.extname(file.originalname);
+    cb(null, `avatar-${Date.now()}${ext}`);
+  }
 });
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image/')) cb(null, true);
-  else cb(new Error('Only image files are allowed'), false);
+  else cb(new Error('Only image files allowed!'), false);
 };
 
-const upload = multer({ storage, fileFilter });
+const uploadBuyerAvatar = multer({ storage, fileFilter, limits: { fileSize: 5 * 1024 * 1024 } });
 
-module.exports = upload;
+module.exports = uploadBuyerAvatar;
