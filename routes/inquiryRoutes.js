1dbf0ff2bb7c6556e4a7981b0009e232d51771b6
pisoftgit <pisoftgit@gmail.com>
2026-03-18 14:24:49 +0530

    inquiries update

--- routes/inquiryRoutes.js
+++ routes/inquiryRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { submitInquiry, getAllInquiries } = require('../controllers/inquiryController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const { protect, restrictTo } = require('../middlewares/authMiddleware');
 
 router.post('/', submitInquiry);
 
-router.get('/', protect, admin, getAllInquiries);
+router.get('/', protect, restrictTo('admin'), getAllInquiries);
 
 module.exports = router;
